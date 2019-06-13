bcc841565c0c14ab251cab85b80e72967212c9f1
Ragavendran Balachandran <586756@cognizant.com>
2019-06-14 02:24:30 +0530

    Embed PowerBI report

--- HotSeatBooking/Controllers/LandingForm.aspx.designer.cs
+++ HotSeatBooking/Controllers/LandingForm.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace HotSeatBooking.Controllers {
         protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
         
         /// <summary>
+        /// HyperLink2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+        
+        /// <summary>
         /// LinkButton1 control.
         /// </summary>
         /// <remarks>
